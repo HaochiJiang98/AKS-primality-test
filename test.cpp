7d75d8389ccada6dfc64cd7a28003edf7b4d9f4b
Claiii(Haochi Jiang) <94361489+claani@users.noreply.github.com>
2023-02-07 15:12:35 +0100

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Ptest.hpp"
 #include "Ptest.cpp"
+#include "time.h"
 using namespace std;
 int main(){
-    int n=9877;
+    unsigned long long n=210002977;
     Ptest test(n);
+    clock_t start_time=clock();
     test.aksTest();
+    clock_t end_time=clock();
+    cout<<"AKS algorithm "<<(double)(end_time-start_time)/CLOCKS_PER_SEC*1000<<" ms "<<endl;
+    start_time=clock();
+    test.trivialTest();
+    end_time=clock();
+    cout<<"Trivial algorithm "<<(double)(end_time-start_time)/CLOCKS_PER_SEC*1000<<" ms "<<endl;
     return 0;
 }
